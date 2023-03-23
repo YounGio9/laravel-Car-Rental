38ea0a749cbc31d19236475eac7d6c8954d3b925
Giovani <desouzagiovani9@gmail.com>
2023-03-23 23:59:04 +0100

    Fixings security for routes

--- resources/views/listings/cars.blade.php
+++ resources/views/listings/cars.blade.php
@@ -3,10 +3,10 @@
     @include('partials._hero')
     @include('partials._search')
 
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 lg:p-12">
+    <div class="lg:grid min-h-[40vh] lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 lg:p-12">
 
         @if (count($listings) === 0)
-            <h2>No Cars found</h2>
+            <h2>Aucune voiture trouvée. Essayez autre vhose </h2>
         @else
             @foreach ($listings as $listing)
                 <x-cars-card :listing="$listing" />
